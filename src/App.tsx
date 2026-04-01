320fe1c3ab97db1a185277a3019f1e50df4acdd7
zdeboer <zackdb2005@gmail.com>
2026-04-01 13:42:13 -0500

    chore: Test CI run

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,10 @@ function App() {
       />
 
       <Card title="Card Title">This is card content.</Card>
+      <div> This is a change div, so I can make a commit for testing purposes.</div>
     </div>
+
+    
   );
 }
 
